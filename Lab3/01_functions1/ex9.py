a81a74822fd1906ff99b7f23934efe76c4708343
Zhantoresh <z_orazymbetov@kbtu.kz>
2024-01-25 17:42:57 +0600

    lab3ex3

---
+++ Lab3/01_functions1/ex9.py
@@ -0,0 +1,9 @@
+"""
+Write a function that computes the volume of a sphere given its radius.
+"""
+import math
+def volume_of_sphere(radius):
+    return (4/3) * math.pi * radius**3
+
+r = float(input("Enter a value of radius: "))
+print("volume of a sphere with radius is:", volume_of_sphere(r))
