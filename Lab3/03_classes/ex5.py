1a8f9413cd34d547e7658557229f1636ab4f7e39
Zhantoresh <z_orazymbetov@kbtu.kz>
2024-02-08 03:08:20 +0600

    for classes

--- Lab3/03_classes/ex5.py
+++ Lab3/03_classes/ex5.py
@@ -8,3 +8,30 @@ and test to make sure the account can't be overdrawn.
 class Account:
     pass
 """
+class Bank:
+    def __init__(self, money):
+        self.balanc = money
+
+    def balance(self):
+        print(f'balance: {self.balanc}')
+
+    def deposit(self, dep):
+        self.balanc += dep
+        print(f'deposit of {dep} successfully made. New balance: {self.balanc}')
+
+    def withdraw(self, wit):
+        if self.balanc < wit:
+            print('insufficient funds!')
+        
+        else:
+            self.balanc -= wit
+            print(f'withdrawal of {wit} successfully made. New balance: {self.balanc}')
+
+
+user = Bank(1000)
+
+user.balance()
+user.deposit(100)
+user.balance()
+user.withdraw(1200)
+user.withdraw(1000)
