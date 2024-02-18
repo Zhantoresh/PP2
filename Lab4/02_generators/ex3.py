b6c8afc909d6275944eda8ac7c88f04a253d5d1a
Zhantoresh <z_orazymbetov@kbtu.kz>
2024-02-18 18:46:04 +0600

    for generators

---
+++ Lab4/02_generators/ex3.py
@@ -0,0 +1,18 @@
+'''
+Define a function with a generator which can iterate the numbers, 
+which are divisible by 3 and 4, between a given range 0 and n.
+'''
+list1 = []
+def generate(n):
+    for i in range(1,n+1):
+        if i%3==0 and i%4==0:
+            yield i
+            list1.append(i)
+n = int(input("Enter:"))
+generate_numbers = generate(n)
+for num in generate_numbers:
+    pass # Пустой цикл, т.к. список list1 заполняется во время итерации по генератору
+if len(list1)==0:
+    print("No such numbers")
+else:
+    print(list1)
