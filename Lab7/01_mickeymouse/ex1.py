2c4386c6a86dbd88ce888d9b5c9eb5900b2baf05
Zhantoresh <z_orazymbetov@kbtu.kz>
2024-03-23 02:31:42 +0600

    for lab7

--- Lab7/01_mickeymouse/ex1.py
+++ Lab7/01_mickeymouse/ex1.py
@@ -4,21 +4,16 @@ which is synchronized with system clock.
 Use Mickey's right hand as minutes arrow and left - as seconds. '''
 import pygame
 import datetime
+
 pygame.init()
 
 WIDTH, HEIGHT = 1200, 1000
-midle = WIDTH // 2, HEIGHT // 2
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+
 
 seconds = pygame.image.load("/Users/1/Desktop/PP2/Lab7/01_mickeymouse/leftarm.png")
 minutes = pygame.image.load("/Users/1/Desktop/PP2/Lab7/01_mickeymouse/rightarm.png")
-rectsec = seconds.get_rect()
-rectmin = minutes.get_rect()
-rectsec.center = midle 
-rectmin.center = midle
-
 
 background = pygame.image.load("/Users/1/Desktop/PP2/Lab7/01_mickeymouse/mainclock.png")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))  
@@ -29,23 +24,23 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    
     time_now = datetime.datetime.now()
     minute_time = time_now.minute 
     second_time = time_now.second
 
-  
-    angle1 = -minute_time * 6 
-    leg1 = pygame.transform.rotate(minutes, angle1)
-    rect1 = leg1.get_rect(center=rectmin.center)
+   
+    minute_angle = -minute_time * 6 
+    second_angle = -second_time * 6 
 
-    angle2 = -second_time * 6  
-    leg2 = pygame.transform.rotate(seconds, angle2)
-    rect2 = leg2.get_rect(center=rectsec.center)
+    
+    rotated_minutes = pygame.transform.rotate(minutes, minute_angle)
+    rotated_seconds = pygame.transform.rotate(seconds, second_angle)
 
     
     screen.blit(background, (0, 0))
-    screen.blit(leg1, rect1)
-    screen.blit(leg2, rect2)
 
+    
+    screen.blit(rotated_minutes, (600 - rotated_minutes.get_width()//2, 500 - rotated_minutes.get_height()//2))  
+    screen.blit(rotated_seconds, (600 - rotated_seconds.get_width()//2, 500 - rotated_seconds.get_height()//2)) #right arm parameters: (63, 1050)
     pygame.display.flip()
-    clock.tick(60)
