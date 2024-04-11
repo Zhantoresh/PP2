087da07417653214dae0da037166f903519a103d
Zhantoresh <z_orazymbetov@kbtu.kz>
2024-04-11 12:14:45 +0600

    for labs

--- Lab8/racer/forlab8.py
+++ Lab8/racer/forlab8.py
@@ -1,3 +1,9 @@
+'''
+1)Adding randomly appearing coins on the road
+2)Showing the number of collected coins in the top right corner
+3)Comment your code
+'''
+
 import pygame
 import random
 import time
--- Lab8/racer/forlab9.py
+++ Lab8/racer/forlab9.py
@@ -1,3 +1,10 @@
+'''
+1)Randomly generating coins with different weights on the road
+2)Increase the speed of Enemy when the player earns N coins
+3)Comment your code
+'''
+
+
 import pygame
 import random
 import time
